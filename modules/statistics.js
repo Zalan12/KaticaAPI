46a3877b9410ed2c64e5d35c057e5ba04eb8d5d0
Zalan12 <farkaszalan@turr.hu>
2025-10-07 06:55:10 +0200

    HF

--- modules/statistics.js
+++ modules/statistics.js
@@ -1,14 +1,52 @@
-const express =require('express');
+
+const express = require('express');
 const router = express.Router();
-const pool=require('../utils/database')
+const pool = require('../utils/database');
 
-//GET minden kell igazabol
-router.get('', (req, res) => {
-    pool.query('SELECT(SELECT COUNT(DISTINCT vevo) from forgalom) as customersCount, (SELECT COUNT(DISTINCT termek) FROM forgalom) AS productsCount, (SELECT SUM(mennyiseg) from forgalom) as salesSum, (SELECT SUM(nettoar)*mennyiseg from forgalom) as priceSums', (error, results) => {
-      if (error)return res.status(500).json({errno:error.errno,msg:'Baj van geco'});
-      res.status(200).json(results);
+router.get('/', (req, res) => {
+    const summaryQuery = `
+      SELECT 
+        COUNT(DISTINCT vevo) AS usersCount, 
+        COUNT(DISTINCT termek) AS productsCount, 
+        SUM(mennyiseg) AS salesSum, 
+        SUM(mennyiseg * nettoar) AS priceSum
+      FROM forgalom;
+    `;
+  
+    const productQuery = `
+      SELECT 
+        termek, 
+        SUM(mennyiseg) AS count, 
+        SUM(mennyiseg * nettoar) AS price
+      FROM forgalom
+      GROUP BY termek;
+    `;
+  
+    pool.query(summaryQuery, (err, summaryResult) => {
+      if (err) return res.status(500).json({ error: err.message });
+  
+      pool.query(productQuery, (err2, productResults) => {
+        if (err2) return res.status(500).json({ error: err2.message });
+  
+        const stats = {
+          usersCount: summaryResult[0].usersCount,
+          productsCount: summaryResult[0].productsCount,
+          salesSum: summaryResult[0].salesSum,
+          priceSum: summaryResult[0].priceSum,
+          products:  productResults.map(row => ({
+              product: row.termek,
+              count: row.count,
+              price: row.price
+            })
+          )
+        };
+  
+       
+  
+        res.json(stats);
+      });
     });
+  });
+  
 
-  })
-
-module.exports=router;
+module.exports = router;
